8fd25c4ff7f3dc92e1c9bf6e120ca97c69c8ae8b
David Hustadt <dh@ai.associates>
2023-07-05 17:55:19 +0200

    custom policy resnet

--- src/utils/custom_policy.py
+++ src/utils/custom_policy.py
@@ -6,11 +6,23 @@ from torch import nn
 
 from stable_baselines3.common.policies import ActorCriticPolicy as acp
 
+import torch
+
+
+class ResNet(torch.nn.Module):
+    def __init__(self, module):
+        super().__init__()
+        self.module = module
+
+    def forward(self, inputs):
+        return self.module(inputs) + inputs
+
 
 class ResidualNetwork(nn.Module):
     def __init__(
         self,
-        feature_dim: int,
+        res_blocks: int = 3,
+        block_dims: List[int] = [64, 64, 64],
         last_layer_dim_pi: int = 64,
         last_layer_dim_vf: int = 64,
     ):
@@ -20,15 +32,35 @@ class ResidualNetwork(nn.Module):
         # Save output dimensions, used to create the distributions
         self.latent_dim_pi = last_layer_dim_pi
         self.latent_dim_vf = last_layer_dim_vf
+        self.res_blocks = res_blocks
+        self.block_dims = block_dims
 
         # Policy network
-        self.policy_net = nn.Sequential(
-            nn.Linear(feature_dim, last_layer_dim_pi), nn.ReLU()
-        )
+        self.policy_net = self.build_res_net(last_layer_dim_pi)
         # Value network
-        self.value_net = nn.Sequential(
-            nn.Linear(feature_dim, last_layer_dim_vf), nn.ReLU()
-        )
+        self.value_net = self.build_res_net(last_layer_dim_vf)
+
+    def build_res_block(
+        self,
+    ) -> nn.Module:
+        # use self.block_dims to create a residual block
+        input_dim = self.block_dims[0]
+        net = []
+        for i in range(1, len(self.block_dims)):
+            output_dim = self.block_dims[i]
+            net.append(nn.Linear(input_dim, output_dim))
+            net.append(nn.ReLU())
+            input_dim = output_dim
+        return ResNet(nn.Sequential(*net))
+
+    def build_res_net(self, last_layer) -> nn.Module:
+        net = []
+        for i in range(self.res_blocks):
+            net.append(self.build_res_block())
+        net.append(nn.Linear(self.block_dims[-1], last_layer))
+        net.append(nn.ReLU())
+        print(net)
+        return nn.Sequential(*net)
 
     def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
         """
