e8e7b81e7b82ac52abf085024f9dea768f899085
David Hustadt <dh@ai.associates>
2023-07-24 22:07:09 +0200

    gsde2

--- src/gsde/gsde_policy.py
+++ src/gsde/gsde_policy.py
@@ -7,8 +7,10 @@ from gymnasium import spaces
 from torch import nn
 
 from src.gsde.basemodel import SB3BaseModel as BaseModel
-from src.gsde.gsde import GeneralizedStateDependentDistribution
-from src.gsde.distribution_helper import get_entropy
+from src.gsde.distribution_helper import get_entropy, sum_independent_dims, get_mean
+from torch.distributions import Normal
+from torch import nn
+from typing import Optional, Tuple
 
 
 class PPO_gSDE_MlpPolicy(BaseModel):
@@ -37,6 +39,32 @@ class PPO_gSDE_MlpPolicy(BaseModel):
         self.ortho_init = ortho_init
 
         self.share_features_extractor = share_features_extractor
+        self.create_feature_extractors()
+
+        self.log_std_init = log_std_init
+        self.should_learn_features = should_learn_features
+        self.dist_kwargs = {
+            "should_learn_features": should_learn_features,
+        }
+
+        self.action_size = int(
+            np.prod(action_space.shape)
+        )  # Number of action dimensions
+        self.latent_sde_dim = None  # Dimension of the latent state dependent noise
+        self.mean_actions = None  # Mean actions (initialized later)
+        self.log_std = None  # Log standard deviation (initialized later)
+        self.weights_dist = None  # Distribution of weights used for exploration
+        self.exploration_mat = None  # Exploration matrix for single sample
+        self.exploration_matrices = None  # Exploration matrices for multiple samples
+        self._latent_sde = None  # Latent state-dependent encoding
+        self.epsilon = 1e-6  # Small epsilon value for numerical stability
+        self.should_learn_features = (
+            should_learn_features  # Flag indicating whether to learn features
+        )
+
+        self._build(lr_schedule)
+
+    def create_feature_extractors(self):
         self.features_extractor = self.make_features_extractor()
         self.features_dim = self.features_extractor.features_dim
         if self.share_features_extractor:
@@ -45,19 +73,6 @@ class PPO_gSDE_MlpPolicy(BaseModel):
         else:
             self.pi_features_extractor = self.features_extractor
             self.vf_features_extractor = self.make_features_extractor()
-
-        self.log_std_init = log_std_init
-        self.should_learn_features = should_learn_features
-        self.dist_kwargs = {
-            "should_learn_features": should_learn_features,
-        }
-
-        self.action_dist = GeneralizedStateDependentDistribution(
-            int(np.prod(action_space.shape)),
-            **self.dist_kwargs,
-        )
-
-        self._build(lr_schedule)
 
     def predict(
         self,
@@ -83,8 +98,28 @@ class PPO_gSDE_MlpPolicy(BaseModel):
 
         return actions, state
 
-    def reset_noise(self, n_envs: int = 1) -> None:
-        self.action_dist.sample_weights(self.log_std, batch_size=n_envs)
+    def reset_noise(self, n_envs: int = 1, log_std=None) -> None:
+        """
+        Samples weights for exploration based on the provided log standard deviation.
+
+        Args:
+            log_std (th.Tensor): Logarithm of the standard deviation.
+            batch_size (int): Number of weight samples to generate. Defaults to 1.
+
+        Returns:
+            None
+        """
+        if log_std is None:
+            log_std = self.log_std
+        std = self.get_std(log_std)
+
+        # Create a distribution with mean zero and the computed standard deviation
+        self.weights_dist = Normal(th.zeros_like(std), std)
+
+        # Sample a single exploration matrix
+        self.exploration_mat = self.weights_dist.rsample()
+        # Sample multiple exploration matrices for batched exploration
+        self.exploration_matrices = self.weights_dist.rsample((n_envs,))
 
     @staticmethod
     def _dummy_schedule(progress_remaining: float) -> float:
@@ -123,7 +158,7 @@ class PPO_gSDE_MlpPolicy(BaseModel):
     def _build(self, lr_schedule) -> None:
         self._build_mlp_extractor()
         latent_dim_pi = self.mlp_extractor.latent_dim_pi
-        self.action_net, self.log_std = self.action_dist.proba_distribution_net(
+        self.action_net, self.log_std = self.proba_distribution_net(
             latent_dim=latent_dim_pi,
             latent_sde_dim=latent_dim_pi,
             log_std_init=self.log_std_init,
@@ -191,9 +226,7 @@ class PPO_gSDE_MlpPolicy(BaseModel):
 
     def _get_action_dist_from_latent(self, latent_pi: th.Tensor):
         mean_actions = self.action_net(latent_pi)
-        return self.action_dist.proba_distribution(
-            mean_actions, self.log_std, latent_pi
-        )
+        return self.proba_distribution(mean_actions, self.log_std, latent_pi)
 
     def _predict(
         self, observation: th.Tensor, deterministic: bool = False
@@ -211,3 +244,166 @@ class PPO_gSDE_MlpPolicy(BaseModel):
         params = super().extract_features(obs, self.vf_features_extractor)
         hidden = self.mlp_extractor.forward_critic(params)
         return self.value_net(hidden)
+
+    def get_actions(self, deterministic: bool = False) -> th.Tensor:
+        if deterministic:
+            return get_mean(self.distribution)
+        return self.sample()
+
+    def get_std(self, log_std: th.Tensor) -> th.Tensor:
+        """
+        Computes the standard deviation from the provided log standard deviation.
+
+        Args:
+            log_std (th.Tensor): Logarithm of the standard deviation.
+
+        Returns:
+            th.Tensor: Standard deviation tensor.
+        """
+
+        # Exponential scaling of standard deviation
+        below_threshold = th.exp(log_std) * (log_std <= 0)
+        safe_log_std = log_std * (log_std > 0) + self.epsilon
+        above_threshold = (th.log1p(safe_log_std) + 1.0) * (log_std > 0)
+        std = below_threshold + above_threshold
+
+        return std
+
+    def proba_distribution_net(
+        self,
+        latent_dim: int,
+        log_std_init: float = -2.0,
+        latent_sde_dim: Optional[int] = None,
+    ) -> Tuple[nn.Module, nn.Parameter]:
+        """
+        Creates a probability distribution network for generating mean actions and log standard deviation.
+
+        Args:
+            latent_dim (int): Dimensionality of the latent space.
+            log_std_init (float): Initial value for the logarithm of the standard deviation.
+                Defaults to -2.0.
+            latent_sde_dim (Optional[int]): Dimensionality of the latent space for the state-dependent encoding.
+                If None, the latent_dim value is used. Defaults to None.
+
+        Returns:
+            Tuple[nn.Module, nn.Parameter]: Tuple containing the mean actions network and the log standard deviation parameter.
+        """
+        mean_actions_net = nn.Linear(latent_dim, self.action_size)
+
+        # Set the latent_sde_dim value to either the provided value or latent_dim
+        self.latent_sde_dim = latent_dim if latent_sde_dim is None else latent_sde_dim
+
+        log_std = th.ones(self.latent_sde_dim, self.action_size)
+        log_std = nn.Parameter(log_std * log_std_init, requires_grad=True)
+
+        # Sample weights for exploration based on the log standard deviation
+        self.reset_noise(log_std=log_std)
+        return mean_actions_net, log_std
+
+    def actions_from_params(
+        self,
+        mean_actions: th.Tensor,
+        log_std: th.Tensor,
+        latent_sde: th.Tensor,
+        deterministic: bool = False,
+    ) -> th.Tensor:
+        """
+        Generates actions based on the mean actions, log standard deviation, and the latent state-dependent encoding.
+
+        Args:
+            mean_actions (th.Tensor): Mean actions tensor.
+            log_std (th.Tensor): Logarithm of the standard deviation tensor.
+            latent_sde (th.Tensor): Latent state-dependent encoding tensor.
+            deterministic (bool): Flag indicating whether to generate deterministic actions.
+                Defaults to False.
+
+        Returns:
+            th.Tensor: Generated actions tensor.
+        """
+        # Construct the probability distribution based on the provided parameters
+        self.proba_distribution(mean_actions, log_std, latent_sde)
+        # Generate actions from the distribution
+        return self.get_actions(deterministic=deterministic)
+
+    def proba_distribution(
+        self, mean_actions: th.Tensor, log_std: th.Tensor, latent_sde: th.Tensor
+    ):
+        """
+        Constructs the probability distribution based on the mean actions, log standard deviation,
+        and the latent state-dependent encoding.
+
+        Args:
+            mean_actions (th.Tensor): Mean actions tensor.
+            log_std (th.Tensor): Logarithm of the standard deviation tensor.
+            latent_sde (th.Tensor): Latent state-dependent encoding tensor.
+
+        Returns:
+            Distribution: Constructed probability distribution.
+        """
+        self._latent_sde = (
+            latent_sde if self.should_learn_features else latent_sde.detach()
+        )
+
+        # Compute the variance based on the latent state-dependent encoding and the standard deviation
+        variance = th.mm(self._latent_sde**2, self.get_std(log_std) ** 2)
+        # Construct the distribution using mean actions and the square root of the variance
+        self.distribution = Normal(mean_actions, th.sqrt(variance + self.epsilon))
+        return self
+
+    def log_prob(self, actions: th.Tensor) -> th.Tensor:
+        """
+        Computes the log probability of actions in the probability distribution.
+
+        Args:
+            actions (th.Tensor): Actions tensor.
+
+        Returns:
+            th.Tensor: Log probability tensor.
+        """
+        gaussian_actions = actions
+
+        # Compute the log probability of the Gaussian actions
+        log_prob = self.distribution.log_prob(gaussian_actions)
+        log_prob = sum_independent_dims(log_prob)
+
+        return log_prob
+
+    def sample(self) -> th.Tensor:
+        noise = self.get_noise(self._latent_sde)
+        actions = self.distribution.mean + noise
+
+        return actions
+
+    def get_noise(self, latent_sde: th.Tensor) -> th.Tensor:
+        """
+        Computes the noise based on the latent state-dependent encoding.
+        """
+        # Detach the latent state-dependent encoding if features are not learned
+        latent_sde = latent_sde if self.should_learn_features else latent_sde.detach()
+        if len(latent_sde) == 1 or len(latent_sde) != len(self.exploration_matrices):
+            return th.mm(latent_sde, self.exploration_mat.to(latent_sde.device))
+        latent_sde = latent_sde.unsqueeze(dim=1)
+        # Compute the noise based on the latent state-dependent encoding and the exploration matrices
+        noise = th.bmm(latent_sde, self.exploration_matrices)
+        return noise.squeeze(dim=1)
+
+    def log_prob_from_params(
+        self, mean_actions: th.Tensor, log_std: th.Tensor, latent_sde: th.Tensor
+    ) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        Computes the log probability of actions based on the mean actions, log standard deviation,
+        and the latent state-dependent encoding.
+
+        Args:
+            mean_actions (th.Tensor): Mean actions tensor.
+            log_std (th.Tensor): Logarithm of the standard deviation tensor.
+            latent_sde (th.Tensor): Latent state-dependent encoding tensor.
+
+        Returns:
+            Tuple[th.Tensor, th.Tensor]: Tuple containing the generated actions tensor and the log probability tensor.
+        """
+        # Generate actions from the provided parameters
+        actions = self.actions_from_params(mean_actions, log_std, latent_sde)
+        # Compute the log probability of the generated actions
+        log_prob = self.log_prob(actions)
+        return actions, log_prob
